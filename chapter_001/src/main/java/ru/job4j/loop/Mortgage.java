13bb36d868f05a22a00fba28e52638223c2e40f0
Ivan88Olegovich <ivanich88wp@mail.ru>
2019-11-30 20:18:23 +0700

    5.7. Ипотека. [#156315]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public int year(double amount, int salary, double percent) {
+        int year = 0;
+        percent=percent/100;
+        while (amount>0){
+            double a= amount+(amount*percent);
+            amount=a-salary;
+            year++;
+
+
+
+        }
+        return year;
+
+}}
