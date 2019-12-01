ed9d9516d2f122650f0c8729cad99f1f7c957c2d
Ivan88Olegovich <ivanich88wp@mail.ru>
2019-12-01 14:39:26 +0700

    5.8. JavaDoc. Code convention

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculate.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+/**
+ * The Calculate program
+ *
+ * @author Ivan Epanchincev (ivanich88wp@vail.ru)
+ * @version 3.0
+ * @since 1.0
+ *
+ */
+
+public class Calculate {
+    /**
+     * Method main
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Package for calculate task
+ *
+ * @author Ivan Epanchincev (ivanich88wp@nail.ru)
+ * @version 3.0
+ * @since 1.0
+ *
+ */
+package ru.job4j.calculator;
