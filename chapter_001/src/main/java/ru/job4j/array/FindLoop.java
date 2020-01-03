c1ba00cedd3e5c89802be73ca9ec4404bfc09ac2
Ivan88Olegovich <ivanich88wp@mail.ru>
2020-01-03 20:39:32 +0700

    6.5. Поиск индекса в диапазоне

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,14 @@ public class FindLoop {
         }
         return rst;
     }
+    public static int indexOf(int [] data,int el,int start, int finish){
+        int rst = -1;
+        for(int index = data[start-1]; index<data[finish];index++){
+            if (data[index]==el){
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
 }
