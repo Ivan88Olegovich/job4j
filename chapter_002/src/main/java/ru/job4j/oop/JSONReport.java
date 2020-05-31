2fc252037fbffb2d46ddad3cd8be8b902473c527
Ivan88Olegovich <ivanich88wp@mail.ru>
2020-05-31 21:30:40 +0700

    4. Переопределение [#184434]

--- chapter_002/src/main/java/ru/job4j/oop/JSONReport.java
+++ chapter_002/src/main/java/ru/job4j/oop/JSONReport.java
@@ -1,8 +1,9 @@
 package ru.job4j.oop;
 
 public class JSONReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
-        return name + System.lineSeparator() + body;
+        return "name: " + name + System.lineSeparator() + "body: " + body;
 
     }
 }
